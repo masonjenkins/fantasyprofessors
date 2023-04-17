9dc189699d6ba0c74543b5536acc6a5ecf99cbc9
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-16 21:10:13 -0500

    login form

--- ui/fantasyprofessors/src/pages/authenticate.js
+++ ui/fantasyprofessors/src/pages/authenticate.js
@@ -1,7 +1,23 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Box from '@mui/material/Box';
+import { Button } from "@mui/material";
+
 
 const Authenticate = props => {
-    return <h2>Auth</h2>
+    return ( 
+    <div>
+    
+        <h2>Auth</h2>
+
+        <Box component={"form"}>
+            <TextField required fullWidth label={"Email"} autoFocus autoComplete="email"></TextField>
+            <TextField required fullWidth label={"Password"} type={"password"} name="password" ></TextField>
+            <Button type="submit" fullWidth variant="contained">Login</Button>
+        </Box>
+    </div>
+    )
 }
 
 export default Authenticate;
