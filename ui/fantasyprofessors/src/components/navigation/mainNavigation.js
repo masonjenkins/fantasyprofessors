14999a0d3c1f4e019c1dbf7af9fbc21b5ddfc901
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-16 19:11:33 -0500

    improved navbar from mui

--- ui/fantasyprofessors/src/components/navigation/mainNavigation.js
+++ ui/fantasyprofessors/src/components/navigation/mainNavigation.js
@@ -1,22 +1,153 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
 import "./mainNavigation.css"
 import MainHeader from "./mainHeader";
 import NavLinks from "./navLinks";
 
 const MainNavigation = props => {
-    return <MainHeader>
-        <button className="menu-button">
-            <span />
-            <span />
-            <span />
-        </button>
-        <h1 className="nav-title"><Link to="/">FantasyProfessors</Link></h1>
-        <nav>
-            <NavLinks />
-        </nav>
-    </MainHeader>
+    // return <MainHeader>
+    //     <button className="menu-button">
+    //         <span />
+    //         <span />
+    //         <span />
+    //     </button>
+    //     <h1 className="nav-title"><Link to="/">FantasyProfessors</Link></h1>
+    //     <nav>
+    //         <NavLinks />
+    //     </nav>
+    // </MainHeader>
+
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+    
+    return (
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: 'none', md: 'flex' },
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                FANTASYPROFESSORS
+              </Typography>
+    
+
+
+
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                {/* <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton> */}
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  <MenuItem key={'Articles'} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Articles</Typography>
+                  </MenuItem>
+                  <MenuItem key={'Login'} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">Login</Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+              <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: 'flex', md: 'none' },
+                  flexGrow: 1,
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                FANTASYPROFESSORS
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+                <NavLink to="/articles">
+                    <Button key="Articles" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>Articles</Button>
+                </NavLink>
+                <NavLink to="/login">
+                    <Button key="Login" onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>Login</Button>
+                </NavLink>
+
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      );
+    
 }
 
 export default MainNavigation;
