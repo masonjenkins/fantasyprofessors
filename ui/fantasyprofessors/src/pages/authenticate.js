a2ac8aab6018a7c4211e404b224499f9960a5d63
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-21 17:00:12 -0500

    ui for auth completed

--- ui/fantasyprofessors/src/pages/authenticate.js
+++ ui/fantasyprofessors/src/pages/authenticate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Box from '@mui/material/Box';
@@ -6,16 +6,56 @@ import { Button } from "@mui/material";
 
 
 const Authenticate = props => {
+
+    const [isLoginMode, setIsLoginMode] = useState(true)
+
+
+    const handleAuthSubmission = event => {
+        event.preventDefault()
+
+        if(isLoginMode) {
+            // do login stuff
+
+        }
+        else {
+            // do signup stuff
+            event.target.name.value = ""
+        }
+
+        event.target.email.value = ""
+        event.target.password.value = ""
+
+    }
+
+    const handleLoginTypeSwitch = () => {
+        if (!isLoginMode) {
+            console.log('pressed')
+            setIsLoginMode(true)
+        }
+        else {
+            console.log('hey')
+            setIsLoginMode(false)
+        }
+    }
+
+
+
     return ( 
     <div>
     
-        <h2>Auth</h2>
+        <h2 style={{textAlign: "center"}}>{isLoginMode? "FantasyProfessors Account Login" : "Create your FantasyProfessors account"}</h2>
 
-        <Box component={"form"}>
-            <TextField required fullWidth label={"Email"} autoFocus autoComplete="email"></TextField>
-            <TextField required fullWidth label={"Password"} type={"password"} name="password" ></TextField>
-            <Button type="submit" fullWidth variant="contained">Login</Button>
-        </Box>
+        <form onSubmit={handleAuthSubmission}>
+            <Box sx={{width: window.innerWidth > 800 ? '50%' : '100%', margin: 'auto', mt: 1}}>
+                {isLoginMode ? <></> : <TextField margin="normal" required id="name" label={"Full Name"} autoFocus type={"text"} fullWidth></TextField>}
+                <TextField margin="normal" required id="email" label={"Email"} autoComplete="email" type={"email"} fullWidth></TextField>
+                <TextField margin="normal" required id="password" label={"Password"} type={"password"} name="password" inputProps={{minLength: 8}} fullWidth></TextField>
+                <Button type="submit"  variant="contained" fullWidth>{isLoginMode ? "Login" : "Signup"}</Button>
+                <Button type="button"  onClick={handleLoginTypeSwitch} fullWidth>{isLoginMode ? "First-timer? SIGNUP" : "Have an account? LOGIN"}</Button>
+            </Box>            
+        </form>
+
+        <h6 style={{textAlign: "center"}}>By {isLoginMode ? "logging in" : "creating an account"}, you agree to the site terms and conditions.</h6>
     </div>
     )
 }
