c695d6eb63411477014963ac0623e0e4542b8804
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-22 10:12:09 -0500

    carousel

--- ui/fantasyprofessors/src/pages/home.js
+++ ui/fantasyprofessors/src/pages/home.js
@@ -1,4 +1,10 @@
 import React from "react";
+import ArticleCard from "../components/articles/articleCard";
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Button } from '@mui/material'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
     const ARTICLES = [
@@ -42,9 +48,29 @@ const Home = () => {
             body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             tags: ["test", "article", "testing"]
         }
+
+        
     ]
+
+    const breakPoints = [{width: 1, itemsToShow: 1}]
+
     return (
-        <h2>Test</h2>
+        <div style={{backgroundColor: 'black'}}>
+            <Carousel>
+                {ARTICLES.map(article => {
+                    return (
+                        <Carousel.Item>
+                            <img className="d-block w-100" src={article.image} alt={article.title} style={{width: '100%', maxWidth: '1200px', maxHeight: '615px', margin: 'auto'}}/>
+                            <Carousel.Caption>
+                                <h5>{article.title}</h5>
+                                <p>{article.teaser}</p>
+                                <Link to={`article/${article._id}`}><Button variant="contained" color="error">View More</Button></Link>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    )
+                })}
+            </Carousel>
+        </div>
     ) 
 }
 
