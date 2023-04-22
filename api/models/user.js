bbbc3078141638d0267a6cb231bab52f8cb8f56d
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-22 14:31:20 -0500

    user schema

---
+++ api/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema 
+
+const userSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    email: { type: String, required: true },
+    name: { type: String, required: true },
+    membership: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true },
+    passwordHash: { type: String, required: true}
+})
+
+module.exports = mongoose.model('User', userSchema)
