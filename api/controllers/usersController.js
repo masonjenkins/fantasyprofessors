c894d20e28f0e33734ffbce13e1650329dcd9aae
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-22 16:04:59 -0500

    users route validation

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,15 +1,24 @@
 const HttpError = require('../models/httpError')
+const { validationResult } = require('express-validator');
 
 const signup = (req, res, next) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return next(new HttpError('Invalid API Call arguments. Try again with correct params.', 400))
+    }
     const { email, name, password } = req.body
 
 
 }
 
 const login = (req, res, next) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return next(new HttpError('Invalid API Call arguments. Try again with correct params.', 400))
+    }
     const { email, password } = req.body 
 
-    
+
 }
 
 exports.signup = signup;
--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
-const usersController = require('../controllers/usersController')
+const usersController = require('../controllers/usersController');
 
-router.post('/login', usersController.login);
+router.post('/login',
+    [check('email').normalizeEmail().isEmail(),
+    check('password').isLength({ min: 8 })
+    ],
+    usersController.login);
 
-router.post('/signup', usersController.signup);
+router.post('/signup',
+    [check('name').not().isEmpty(),
+    check('email').normalizeEmail().isEmail(),
+    check('password').isLength({ min: 8 })
+],
+ usersController.signup);
 
 module.exports = router;
