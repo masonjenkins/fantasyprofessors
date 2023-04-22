7dce245283d71a4e6c0b64ca5901ba10316697e4
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-22 11:49:47 -0500

    individual article page done

--- ui/fantasyprofessors/src/components/article/articleComponent.js
+++ ui/fantasyprofessors/src/components/article/articleComponent.js
@@ -8,15 +8,18 @@ const ArticleComponent = props => {
 
         
     <div>
-        <img src={props.item.image} alt={props.item.title} />
-
-        <div>
+        <br />
+        <div style={{backgroundColor: 'black'}}>
+            <img src={props.item.image} style={{maxWidth: '75%', marginLeft: 'auto', marginRight: 'auto', display: 'block'}} />
+        </div>
+        <br />
+        <div style={{textAlign: 'center'}}>
             <h2>{props.item.title}</h2>
             <h4>{props.item.author}</h4>
             <h5>{props.item.date}</h5>
         </div>
 
-        <div>
+        <div className="container">
             <p>{props.item.body}</p>
         </div>
     </div>
