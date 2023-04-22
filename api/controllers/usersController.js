1e79c2d88512425130e08a6ed009b64a8afbef1f
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-22 14:31:48 -0500

    users routes

---
+++ api/controllers/usersController.js
@@ -0,0 +1,12 @@
+const HttpError = require('../models/httpError')
+
+const signup = (req, res, next) => {
+
+}
+
+const login = (req, res, next) => {
+
+}
+
+exports.signup = signup;
+exports.login = login;
---
+++ api/routes/usersRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/usersController')
+
+router.post('/login', usersController.login);
+
+router.post('/signup', usersController.signup);
+
+module.exports = router;
