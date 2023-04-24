1fbaa67b9cf1f5158ee5c92862629fe4ac974e8f
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-23 22:49:53 -0500

    store token in localstorage

--- ui/fantasyprofessors/src/App.js
+++ ui/fantasyprofessors/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { AuthContext } from './context/authenticationContext';
 import Home from './pages/home';
@@ -8,19 +8,45 @@ import MainNavigation from './components/navigation/mainNavigation';
 import Authenticate from './pages/authenticate';
 import CreateArticle from './pages/createArticle';
 
+let logoutTimer
+
 function App() {
   const [token, setToken] = useState(null)
   const [isAdmin, setIsAdmin] = useState(false)
+  const [tokenExpiration, setTokenExpiration] = useState()
 
-  const login = useCallback((token, adminStatus) => {
+
+  const login = useCallback((token, adminStatus, expiration) => {
     setIsAdmin(adminStatus)
     setToken(token)
+    const tokenExpiration = expiration || new Date(new Date().getTime() + 86400000)
+    setTokenExpiration(tokenExpiration)
+    localStorage.setItem('userData', JSON.stringify({ isAdmin: adminStatus, token: token, expiration: tokenExpiration.toISOString() }))
   }, [])
 
   const logout = useCallback(() => {
     setIsAdmin(false)
     setToken(null)
+    setTokenExpiration(null)
+    localStorage.removeItem('userData')
   }, [])
+
+  useEffect(() => {
+    const tokenRemainingTime = tokenExpiration.getTime() - new Date().getTime()
+    if(token) {
+      logoutTimer = setTimeout(logout, tokenRemainingTime)
+    } else {
+      clearTimeout(logoutTimer)
+    }
+  }, [token, logout, tokenExpiration])
+
+  useEffect(() => {
+    let userData = JSON.parse(localStorage.getItem('userData'))
+    
+    if(userData && userData.token && new Date(userData.expiration) > new Date()) {
+      login(userData.token, userData.isAdmin, new Date(userData.expiration))
+    }
+  }, [login])
 
 
   let routes 
