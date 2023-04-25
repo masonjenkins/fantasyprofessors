14e3685abf9023ab6a7affc71d73a343041b1f92
Mason Jenkins <mason.jenkins00@gmail.com>
2023-04-24 21:19:27 -0500

    change port

--- api/app.js
+++ api/app.js
@@ -40,5 +40,5 @@ app.use((error, req, res, next) => {
 
 mongoose
     .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.shqmdcm.mongodb.net/${process.env.MONGO_NAME}?retryWrites=true&w=majority`)
-    .then(() => {app.listen(3000)})
+    .then(() => {app.listen(5000)})
     .catch(e => {console.log(e)})
